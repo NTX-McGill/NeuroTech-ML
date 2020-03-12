2b743b566753857d55d1b020c7ec96753ace6ed4
miasya <41090034+miasya@users.noreply.github.com>
2020-03-12 11:25:20 -0400

    converting to using match_labels.py windowsing - plz see note for issues

--- miasya.py
+++ miasya.py
@@ -7,21 +7,16 @@ Created on Sun Mar  8 16:49:41 2020
 
 """
 
-- take data windows of 1s (use rolands code) but with no excess info
-- Featurize
-- put it through model, get prediction
-- get most likely keypress/finger (if >0.5)
-- plot it as markings on the stacked signal windows
-- make the visual good
-# look into eventplot matplotlib
-# add actual
-# all channels 1 graph
-# two hands
-# show actual letter
-# show number that corresponds to that letter
-# standardize the axes
+# DONE:
+# take data windows of 1s (use rolands code) but with no excess info
+# Featurize
+# put in model, get prediction ie most likely keypress/finger (if >0.5)
+# plot predictions as markings on the stacked signal windows
 
-# match the colours in hand plots, also separate finger into 2 subplots?
+# TO DO:
+# add actual labels (number and letter)
+# Match the colours of finger label to channel
+# standardize the axes and general increase readability
 
 """
 
@@ -31,20 +26,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 channels = [1,2,3,4,5,6,7,8]
-    
+length = 250
+shift = 0.1
+
+channel_names = ['channel {}'.format(i) for i in channels]
+feature_names = ['mav']
 #%%
+data_file = 'data/2020-03-08/012_trial2_self-directed_OpenBCI-RAW-2020-03-08_19-02-54.txt'
+label_file = 'data/2020-03-08/012_trial2_self-directed_2020-03-08-19-06-09-042.txt'
+
+res = append_labels(data_file, label_file, channels)
+#%%
+data = filter_dataframe(res)
+#%%
+windows = label_window(data)
+#%%
+"""
+
+OLD PARSING - uses an OPENBCI file with json manually removed
+
 data_file = 'miasya_data_test.txt'
-    
+
 raw_data = np.loadtxt(data_file,
                       delimiter=',',
                       skiprows=7,
                       usecols=channels)
-#%%
+
 data = np.zeros((raw_data.shape))
 
+# Note this requires import
 for ch in range(raw_data.shape[1]):
      data[:,ch] = filter_signal(raw_data[:,ch])
-
+    
 #%%
 length = 250 # 1.00 seconds
 shift = 50  # 0.20 seconds
@@ -60,8 +73,12 @@ for i in range(0, data.shape[0]-length, shift):
         w[j] = data[i:i+length, j]
             
     windows.append(w)
-    
+"""
 #%%
+"""
+
+More old stuff
+
 # Windows is a list, where every entry of the list is a 250 by 8 numpy ndarry
 # that represents the 250 data points for each of the 8 channels
 # The time of the occurance is related to the index of within the windows list
@@ -72,18 +89,29 @@ for i in range(0, data.shape[0]-length, shift):
 #plt.plot(x,windows[0][0,:])
 
 # Now to featurize each of the windoes individually
+"""
 
-# Convert to pandas dataframe
-
-#feature_names = ['mav']
-#channel_names = ['channel {}'.format(i) for i in channels]
-    
-rt = RealTimeML()
-
+#%% 
+ML = RealTimeML()
 all_predictions = []
+
+# for now we make windows into a numpy array cause it's not liking the df
+
+# old format that previously worked:
+# Windows is a list, where every entry of the list is a 250 by 8 numpy ndarry
+# that represents the 250 data points for each of the 8 channels
+
+# ok lets convert
+windows = windows[channel_names]
+
+""" PSEUDOCODE OF WHAT WE NEED TO DO TO FIX IT -- PLZ REPAIR ME
+windows_list = []
+for row in windows:
+    windows_list.append(windows[row].to_numpy())
+"""
+
 for win in windows:   
-    all_predictions.append(rt.predict_function(win))
-    
+    all_predictions.append(ML.predict_function(win))
     
 #%%
 # now we have the predictions, and we need to plot it against all the channels
@@ -109,6 +137,7 @@ plt.title('hand two')
 # pred val holds whichever index is greatest and has a probability > 0.5
 pred_vals = np.zeros((int((data.shape[0]-length)/shift)+1,10))
 
+# This gets the timestamps we need for the eventplot later
 time = 0
 time_index = 0
 for pred in all_predictions:
@@ -118,9 +147,7 @@ for pred in all_predictions:
     time += shift
     time_index += 1
     
-    
 #%%
-    
 # on last subplot show events for each classification
 color = ['b','g','r','c','m','y','k','Crimson', 'DarkOrange', 'SpringGreen']
 plt.subplot(3,1,3)
@@ -129,30 +156,17 @@ plt.subplot(3,1,3)
 fitted_pred_vals = pred_vals[int(start/shift):int(end/shift)];
 fitted_pred_vals[fitted_pred_vals != 0] -= start;
 #%%
+
+# Now make eventplot
 for clas in range(pred_vals.shape[1]):
     plt.eventplot(fitted_pred_vals[:,clas], colors=color[clas])
 
     # now put the labels, markers is marker times, so the x position?
     markers = fitted_pred_vals[:,clas][fitted_pred_vals[:,clas] != 0]
     
+    # and put markers of uniform colour above event lines
     for m in markers:
         plt.text(int(m),1,str(clas))
-    
 #%%
-# now show actual marking
-# i need this from rolands stuff
-data_file = 'data/2020-03-08/012_trial2_self-directed_OpenBCI-RAW-2020-03-08_19-02-54.txt'
-label_file = 'data/2020-03-08/012_trial2_self-directed_2020-03-08-19-06-09-042.txt'
-
-res = append_labels(data_file, label_file, channels)
-#%%
-labels = res[['timestamp(ms)','finger','keypressed']]
-raw_data = res[['channel 1','channel 2','channel 3','channel 4','channel 5',
-            'channel 6','channel 7','channel 8']]
-
-#%%
-data = filter_dataframe(res)
-#%%
-windows = label_window(data)
-        
-#plt.show()
+# Uncomment to maybe use later for plotting
+#labels = res[['timestamp(ms)','finger','keypressed']]
