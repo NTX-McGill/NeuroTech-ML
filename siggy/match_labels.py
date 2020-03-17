52c2815ea02e7e8219b7346a8f54d172f8ecba60
Roland <roland@riachi.com>
2020-03-16 22:22:41 -0400

    Made code readable

--- siggy/match_labels.py
+++ siggy/match_labels.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from glob import glob
 from scipy import signal
+from constants import LABEL_MAP, HAND_MAP, HAND_FINGER_MAP, MODE_MAP, SAMPLING_FREQ
 import json
 
 
@@ -120,7 +121,84 @@ def filter_dataframe(df,filter_type='original_filter'):
         
     return filtered_df
 
-def append_labels(data_file, label_file, channels):
+def get_metadata(label_file):
+    """
+    Converts the JSON at the beginning of input to a dict, and converts id/prompts values to int and list, respectively.
+    
+    Parameters
+    ----------
+    label_file : str
+        Name of file containing metadata for data trial
+
+    Returns
+    -------
+    meta : dict
+        The metadata about the data associated to the input
+    """
+    
+    #As a convention the first 9 lines of each labels file is a JSON containing metadata about the trial
+    #Read the metadata lines and convert them into a dict
+    with open(label_file) as f:
+        meta_string = ''.join(f.readlines()[:9])
+        meta = json.loads(meta_string)
+    
+    #Convert subject id from STR to INT
+    meta['id'] = int(meta['id']) 
+    
+    #Convert prompts from STR to LIST
+    meta['prompts'] = [s.strip() for s in meta['prompts'].split(',')]
+    
+    return meta
+
+def init_labelled_df(data, names):
+    """
+    Pre-allocates DataFrame for labelled datapoints, values are by default set to np.NaN.
+
+    Parameters
+    ----------
+    data : ndarray
+        Numpy ndarry of values read from data file.
+    names : list
+        List of columns names for output DataFrame.
+
+    Returns
+    -------
+    labelled_data : DataFrame
+        Pandas DataFrame with shape (# of data points, # of channels + # of labels).
+    """
+    
+    labelled_data = pd.DataFrame(data=np.full((len(data), len(names)), np.NaN), columns=names)
+    labelled_data.iloc[:, :9] = data
+    
+    return labelled_data
+    
+def to_hand(inp):
+    """
+    Computes (uninteresting, cheap math trick) encoding for hand used to press key, given which key was pressed.
+
+    Parameters
+    ----------
+    inp : str, int, or np.NaN
+        Key that was pressed.
+
+    Returns
+    -------
+    int
+        Encoding of hand that pressed key.
+
+    """
+
+    if type(inp) == str:
+        return (LABEL_MAP[inp] - 1) // 5 + 1
+    elif type(inp) == int:
+        return (inp - 1) // 5 + 1
+    elif np.isnan(inp):
+        return inp
+    else:
+        print("Unhandled type:", type(inp))
+        raise Exception
+
+def append_labels(data_file, label_file, channels=[1,2,3,4,5,6,7,8]):
     """
         Append ASCII values of labels in keyboard markings file to nearest (in terms of time) 
         data point from data set
@@ -130,17 +208,6 @@ def append_labels(data_file, label_file, channels):
         outputs:
           labelled_data (DataFrame)
     """
-    
-    #Constants
-    mode_legend = {'Guided': 1, 'Self-directed': 2, 'In-the-air': 3}    
-    hand_legend = {'left': 1, 'right': 2}
-    hand_finger_legend = {'left' : {'thumb': 6, 'index finger': 7, 'middle finger': 8, 'ring finger': 9, 'pinkie': 10},
-                          'right': {'thumb': 1, 'index finger': 2, 'middle finger': 3, 'ring finger': 4, 'pinkie': 5}}
-    LABEL_MAP = {'1':10, '2':9, '3':8, '4':7, '5':7, '6': 2, '7':2, '8':3, '9':4, '0': 5,
-             'q':10, 'w':9, 'e':8, 'r':7, 't':7, 'y':2, 'u':2, 'i':3, 'o':4, 'p':5,
-             'a':10, 's':9, 'd':8, 'f':7, 'g':7, 'h':2, 'j':2, 'k':3, 'l':4, ';':5,
-             'z':10, 'x':9, 'c':8, 'v':7, 'b':7, 'n':2, 'm':2, ',':3, '.':4, '/':5,
-             '[':5, ']':5, "'":5, '\\':5 , 'space': 1, 'Shift': 10, 'Backspace':5}
     
     #Load data from files
     data = np.loadtxt(data_file,
@@ -154,30 +221,24 @@ def append_labels(data_file, label_file, channels):
                          header=None, 
                          engine='python')
     
-    #Parse metadata at start of file for patient id and mode of data collection
-    with open(label_file) as f:
-        meta_string = ''.join(f.readlines()[:9])
-        meta = json.loads(meta_string)
-        idx = int(meta['id'])
-        mode = mode_legend[meta['mode']]
+    #Get useful metadata
+    meta = get_metadata(label_file)
+    subject_id, trial_mode = meta['id'], MODE_MAP[meta['mode']]
     
-    #Get useful columns
-    emg = data[:, :-1]
+    #Get timestamps and labels
     data_timestamps = data[:, -1]
     label_timestamps = labels['timestamp(ms)']
-    
-    #Get labels
     hands = labels['hand']
     fingers = labels['finger']
     keys = labels['keypressed']
     
-    #Map labels to a column of data
-    #Initialize label Series to NaN, subject id, data collection mode
-    hand_labels = pd.Series(np.full(len(emg), np.NaN))
-    finger_labels = pd.Series(np.full(len(emg), np.NaN))
-    key_labels = pd.Series(np.full(len(emg), np.NaN))
-    id_labels = pd.Series(np.full(len(emg), idx))
-    mode_labels = pd.Series(np.full(len(emg), mode))
+    #Pre-allocate new DataFrame
+    names = ['channel {}'.format(i) for i in channels] + ['timestamp(ms)', 'hand', 'finger', 'keypressed', 'id', 'mode']
+    labelled_data = init_labelled_df(data, names)
+    
+    #Initialize values for id, mode
+    labelled_data['id'][:] = subject_id
+    labelled_data['mode'][:] = trial_mode
     
     #Append each label to nearest timestamp in data
     for i in range(len(label_timestamps)):
@@ -187,65 +248,89 @@ def append_labels(data_file, label_file, channels):
         #... prompt_end, left, index finger,   <-- Example of labels in "prompt_end" line
         #... keystroke, , , k                  <-- Example of labels in non-"prompt_end" line
         if any(keys.notnull()):
-            #Ignore "prompt_end" lines
             if keys[i]: 
-                hand_labels[ind], finger_labels[ind], key_labels[ind] = (LABEL_MAP[keys[i]] - 1) // 5 + 1, LABEL_MAP[keys[i]], keys[i]    
+                    labelled_data['hand'][ind] = to_hand(keys[i])
+                    labelled_data['finger'][ind] = LABEL_MAP[keys[i]]
+                    labelled_data['keypressed'][ind] = keys[i]
         else:
-            hand_labels[ind], finger_labels[ind] = hand_legend[hands[i]], hand_finger_legend[hands[i]][fingers[i][:-1]]
-    
-    #Put everything into a DataFrame
-    names = ["channel " + str(i) for i in range(1, data.shape[1])] + ['timestamp(ms)']
-    labelled_data = pd.DataFrame(data, columns=names)
-    labelled_data['hand'] = hand_labels
-    labelled_data['finger'] = finger_labels
-    labelled_data['keypressed'] = key_labels
-    labelled_data['id'] = id_labels
-    labelled_data['mode'] = mode_labels
-    
+            labelled_data['hand'][ind] = HAND_MAP[hands[i]]
+            labelled_data['finger'][ind] =  HAND_FINGER_MAP[hands[i]][fingers[i][:-1]]
+
     return labelled_data
+
+def get_window_label(labels, win_start, win_len):
+    """
+    Gets the label in closest to the middle index of a window, returns np.NaN if there are no events in the window
+
+    Parameters
+    ----------
+    labels : Series
+        Labels of the events in the window (either finger or key press label).
+    win_labels : Series
+        DESCRIPTION.
+    win_start : int
+        Starting index of the window.
+    win_len : int
+        Length of the window.
+
+    Returns
+    -------
+    int (if labels if a list of ints ) or str (if labels is a list of strings) <- I know this is bad practice, will change
+        DESCRIPTION.
+
+    """
+    
+    if len(labels) == 0:
+        return np.NaN
+    else:
+        mid_ind = (2*win_start + win_len)//2
+        indices = np.array(labels.index)
+        
+        return labels.iloc[np.argmin(np.abs(indices - mid_ind))]
 
 def label_window(data, length=1, shift=0.1, offset=2, take_everything=False):
     """
         Combines data points from data into labelled windows
         inputs:
-            data    (DataFrame)
+            data    (DataFrame)indices = np.array([np.where(key_labels.iloc[i:i+length] == l)[0][0] for l in w_key_labels])
             length  (int)
             shift   (int)
             offset  (int)
         output:
             windows (DataFrame) 
     """
-    
-    SAMPLING_FREQ = 250
-    
+
     #Convert arguments
     length, shift, offset = int(length*SAMPLING_FREQ), int(shift*SAMPLING_FREQ), int(offset*SAMPLING_FREQ)
     
-    #Check whether to look for keypressed or hand/fingers as labels
-    have_keys = True if any(data['keypressed'].notnull()) else False
+    #If any key press labels are not null, label windows using key presses - otherwise label using fingers
+    label_col = 'keypressed' if any(data['keypressed'].notnull()) else 'finger'
 
     #Find starting index
-    if have_keys:
-        start = np.where(data['keypressed'].notnull())[0][0]
-        end = np.where(data['keypressed'].notnull())[0][-1]
-    else:
-        start = np.where(data['hand'].notnull())[0][0]
-        end = np.where(data['hand'].notnull())[0][-1]
     if take_everything:
         start = 0
         end = len(data)
+    else:
+        start = np.where(data[label_col].notnull())[0][0]
+        end = np.where(data[label_col].notnull())[0][-1]
     
+    #Find how many channels are used in the dataframe by looking at the names of the columns
     ch_ind = []
     for i in range(len(data.columns)):
-        if 'channel' in data.columns[i]: ch_ind.append(i)
+        if 'channel' in data.columns[i]: 
+            ch_ind.append(i)
     
+    #Only focus on the part of the emg data between start and end
     emg = data.iloc[start - offset:end + offset, ch_ind]
     
-    #Create windows with labels
+    #Create and label windows
     windows = []
-    window_hand_labels, window_finger_labels, window_key_labels = [], [], []
+    labels = data[label_col][start - offset: end + 1 + offset]
+    window_labels = []
     for i in range(0, emg.shape[0], shift):
         #Handle windowing the data
+        #Want w to be a list of ndarrays representing channels, 
+        #so w = np.array(emg[i: i+length, :]) doesn't work (it gives array with shape (250, 8))
         w = []
         for j in range(emg.shape[1]):
             channel = emg.iloc[i:i+length, j]
@@ -254,58 +339,32 @@ def label_window(data, length=1, shift=0.1, offset=2, take_everything=False):
         #Only use windows with enough data points
         if len(w[0]) != length: continue
         
+        #
         windows.append(w)
-
-        #Handle the labels of the windows
-        if have_keys:
-            key_labels = data['keypressed'][start - offset: end + offset]
-            w_key_labels = key_labels[i:i+length][key_labels[i:i+length].notnull()]
-
-            if len(w_key_labels) == 0: #No keys pressed
-                window_key_labels.append(np.NaN)
-            elif len(w_key_labels) == 1: #One key pressed
-                window_key_labels.append(w_key_labels.iloc[0]) 
-            else: #If more than one keypressed in window, take closest one to middle of window
-                indices = np.array([np.where(key_labels.iloc[i:i+length] == l)[0][0] for l in w_key_labels])
-                mid_ind = (2*i + length)//2
-                window_key_labels.append(w_key_labels.iloc[np.argmin(np.abs(indices - mid_ind))])
-        else:
-            hand_labels = data['hand'][start - offset:end + offset]
-            finger_labels = data['finger'][start - offset:end + offset]
-            w_hand_labels = hand_labels[i:i+length][hand_labels[i:i+length].notnull()]
-            w_finger_labels = finger_labels[i:i+length][finger_labels[i:i+length].notnull()]
-
-            if len(w_hand_labels) == 0: #No keys pressed
-                window_hand_labels.append(np.NaN)
-                window_finger_labels.append(np.NaN)
-            elif len(w_hand_labels) == 1: #One key pressed
-                window_hand_labels.append(w_hand_labels.iloc[0]) 
-                window_finger_labels.append(w_finger_labels.iloc[0])
-            else: #If more than one keypressed in window, take closest one to middle of window
-                indices = np.array([np.where(hand_labels.iloc[i:i+length] == l)[0][0] for l in w_hand_labels])
-                mid_ind = (2*i + length)//2
-                window_hand_labels.append(w_hand_labels.iloc[np.argmin(np.abs(indices - mid_ind))])
-                window_finger_labels.append(w_hand_labels.iloc[np.argmin(np.abs(indices - mid_ind))])
+    
+        #Get all not-null labels in the window (if any) and choose which one to use for the window
+        w_labels = labels[i: i + length][labels[i: i + length].notnull()]
+        window_labels.append(get_window_label(labels, w_labels, i, length))
     
     #Put everything into a DataFrame
-    names = [data.columns[i] for i in ch_ind]
-    windows_df = pd.DataFrame(windows, columns=names)
+    channel_names = [data.columns[i] for i in ch_ind]
+    windows_df = pd.DataFrame(windows, columns=channel_names)
     
-    if have_keys:
-        windows_df['hand'] = pd.Series(np.full(len(windows), np.NaN))
-        windows_df['finger'] = pd.Series(np.full(len(windows), np.NaN))
-        windows_df['keypressed'] = pd.Series(window_key_labels)
+    window_labels_series = pd.Series(window_labels)
+    if label_col == 'keypressed':
+        windows_df['hand'] = window_labels_series.apply(to_hand)   #Map key presses to hand
+        windows_df['finger'] = window_labels_series.map(LABEL_MAP) #Map key presses to fingers
+        windows_df['keypressed'] = window_labels_series            #Keep labels as they are
     else:
-        windows_df['hand'] = pd.Series(window_hand_labels)
-        windows_df['finger'] = pd.Series(window_finger_labels)
-        windows_df['keypressed'] = pd.Series(np.full(len(windows), np.NaN))
+        windows_df['hand'] = window_labels_series.apply(to_hand)            #Map finger to hand
+        windows_df['finger'] = window_labels_series                         #Kepp labels as they are
+        windows_df['keypressed'] = pd.Series(np.full(len(windows), np.NaN)) #No key presses
     
+    #All the windows have the same id and mode as labelled data
     windows_df['id'] = pd.Series(np.full(len(windows), data['id'][0]))
     windows_df['mode'] = pd.Series(np.full(len(windows), data['mode'][0]))
     
     return windows_df
-
-
 
 def merge_data(directory, channels, filter_type='original_filter', file_regex='*.txt'):
     """
@@ -328,10 +387,6 @@ def merge_data(directory, channels, filter_type='original_filter', file_regex='*
         print("Appending trial with labels:", files[i])
         data = append_labels(files[i+1], files[i], channels)
         
-        if data.empty:
-            print("Not in air, skipping file!")
-            continue
-        
         # Filter data
         data = filter_dataframe(data,filter_type=filter_type)
         
@@ -351,17 +406,19 @@ def merge_data(directory, channels, filter_type='original_filter', file_regex='*
     
     return big_data, windows
 
+#Can still abstract pre-allocating and initilizing DataFrames, will do that later if time permitting
+
 if __name__ == '__main__':
     #Testing code
     channels = [1,2,3,4,5,6,7,8]
     
-#    markers = '../data/2020-02-23/002-trial1-both-guided-2020-02-23-18-16-45-254.txt'
-#    fname = '../data/2020-02-23/002-trial1-both-guided-OpenBCI-RAW-2020-02-23_18-14-32.txt'
-#    test = append_labels(fname, markers, channels)
-#    out = label_window(test)
+    markers = '../data/2020-02-23/002-trial1-both-guided-2020-02-23-18-16-45-254.txt'
+    fname = '../data/2020-02-23/002-trial1-both-guided-OpenBCI-RAW-2020-02-23_18-14-32.txt'
+    test = append_labels(fname, markers, channels)
+    out = label_window(test)
     
-    directory = '../data/2020-02-23/'
-    labelled_raw, good_windows = merge_data(directory, channels)    
+    # directory = '../data/2020-02-23/'
+    # labelled_raw, good_windows = merge_data(directory, channels)    
 #     windows.to_csv('windows-2020-02-23.csv', index=False)
     # good_windows.to_pickle('windows-2020-02-23_not_real_time.pkl')
 
