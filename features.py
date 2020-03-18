e976eb37680125f91d090c520eeadd75ba3e483a
dcxSt <stephen.fay@mail.mcgill.ca>
2020-03-18 12:10:01 -0400

    fixed certain features, in particular freq vars

--- features.py
+++ features.py
@@ -16,14 +16,20 @@ def silly(signal):
     return np.asarray(signal[:3])
 
 
+#### 
 
-#Integrated IEMG
 def iemg(signal):
-    return np.sum(np.abs(signal))
+    return np.mean(np.abs(signal))
+
+def wfl(signal):
+    return wl(signal)
+
+####
+
 
 #Mean absolute value
 def mav(signal):
-    return np.sum(np.abs(signal)) / len(signal)
+    return np.mean(np.abs(signal)) 
 
 #Modified mean absolute value
 def mmav(signal):
@@ -33,7 +39,7 @@ def mmav(signal):
         else: return 4*(n - N)/N
     
     w = np.array([weights(n, len(signal)) for n in range(len(signal))])
-    return np.dot(w, np.abs(signal)) / len(signal)
+    return np.dot(w, np.abs(signal)) / len(signal) 
 
 #Modified mean abs value 2
 def mmav2(signal):
@@ -41,21 +47,20 @@ def mmav2(signal):
         if n < .33*N or n > .67*N: return .33
         else: return .67
     w = np.array([weights(n, len(signal)) for n in range(len(signal))])
-    return np.dot(w, np.abs(signal)) / len(signal)
+    return np.dot(w, np.abs(signal)) / len(signal) 
 
-#
 
 #Variance
 def var(signal):
-    return np.sum(np.square(signal)) / (len(signal) - 1)
+    return np.mean(np.square(signal - np.array([np.mean(signal)]*len(signal)))) 
 
 # abs value varience
 def var_abs(signal):
-    return np.sum(np.square(np.abs(signal) - np.array(np.mean(np.abs(signal))*len(signal)))) / (len(signal) - 1)
+    return np.mean(np.square(np.abs(signal) - np.array([np.mean(np.abs(signal))]*len(signal))))
 
 #Root mean square
 def rms(signal):
-    return np.sqrt(np.sum(np.square(signal)) / len(signal))
+    return np.sqrt(np.sum(np.square(signal)) / len(signal)) 
 
 
 #In what follows, thresholds: 10-100mV
@@ -67,28 +72,30 @@ def zc(signal, threshold=40):
     signal -= np.mean(signal)
     
     shifted_signal = np.append(signal[1:], signal[-1])
-    return len(signal[ (signal*shifted_signal < 0) & (np.abs(signal-shifted_signal) >= threshold)])
+    return len(signal[ (signal*shifted_signal < 0) & (np.abs(signal-shifted_signal) >= threshold)]) 
 
 #Willison amplitude
 def wamp(signal, threshold=20):
     shifted_signal = np.append(signal[1:], signal[-1])
-    return len(signal[ np.abs(signal-shifted_signal) >= threshold])
+    return len(signal[ np.abs(signal-shifted_signal) >= threshold]) 
 
 def wl(signal):
     signal = np.array(signal)
     shifted_signal = np.append(signal[1:], signal[-1])
-    return np.sum(np.abs(signal-shifted_signal))
+    return np.mean(np.abs(signal-shifted_signal)) 
 
-# slope sign change
+# slope sign change - maybe add a threshold...
 def ssch(signal):
     def is_neg(a):
         if a<0: return 1
         else: return 0
     return np.sum([is_neg(signal[i]*signal[i+1]) for i in range(len(signal)-1)])
 
-#The Length of the Waveform Per Unit
-def wfl(signal):
-    return np.sum([np.abs(signal[i]-signal[i+1]) for i in range(len(signal)-1)]) / len(signal)
+### subinterval features
+#Root mean squared subwindows
+def rms_3(signal):
+    signal_split = [signal[:int(len(signal)/3)],signal[int(len(signal)/3):int(2*len(signal)/3)],signal[int(2*len(signal)/3):]]
+    return [np.sqrt(np.sum(np.square(i))) / len(signal_split[0]) for i in signal_split] #these are already normalized, the values are roughly 1 for each of them
 
 #Spectrogram
 
@@ -103,17 +110,19 @@ def freq_feats(signal):
         print('we need to fix this : the window is too short')
         return [1,2,3,4,5,6]
     psd = get_psd(signal)
-    return [np.mean(psd[5:20]),np.mean(psd[20:40]),np.mean(psd[40:60]),np.mean(psd[60:80]),np.mean(psd[80:100]),np.mean(psd[100:120])]    
+    return [np.mean(psd[5:20])  , np.mean(psd[20:40]) , 
+            np.mean(psd[40:60]) , np.mean(psd[60:80]) ,
+            np.mean(psd[80:100]) , np.mean(psd[100:120])]    
 
-# some more freq domain features
+# some more freq domain features - michell : take out the high freq stuff
 def freq_var(signal):
     if len(signal) < 250:
         print('aaaaaaahhhhhhhhh hell : the window is too short')
         return np.asarray([1,2,3])
     psd = get_psd(signal)
-    return np.asarray([np.mean(np.abs(psd[:40] - np.array([np.mean(psd[:40])]*40))),
-                       np.mean(np.abs(psd[40:80] - np.array([np.mean(psd[40:80])]*20))),
-                               np.mean(np.abs(psd[80:] - np.array([np.mean(psd[80:])]*len(psd[80:]))))])
+    return np.asarray([np.mean(np.abs(psd[:40] - np.array([np.mean(psd[:40])]*40))) ,
+                       np.mean(np.abs(psd[40:80] - np.array([np.mean(psd[40:80])]*40))) ,
+                               np.mean(np.abs(psd[80:] - np.array([np.mean(psd[80:])]*len(psd[80:])))) ])
 
 # more freq domain features
 def freq_misc(signal):
@@ -124,11 +133,6 @@ def freq_misc(signal):
     return [ssch(psd),mav(psd),mmav(psd),zc(psd-[.5]*len(psd))]
 
 
-### subinterval features
-#Root mean squared subwindows
-def rms_3(signal):
-    signal_split = [signal[:int(len(signal)/3)],signal[int(len(signal)/3):int(2*len(signal)/3)],signal[int(2*len(signal)/3):]]
-    return [np.sqrt(np.sum(np.square(i))) / len(signal_split[0]) for i in signal_split]
 
 
 
