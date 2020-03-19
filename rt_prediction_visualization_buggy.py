b281f68c033c353f2fefe37fd771bfeeb1e8be15
miasya <41090034+miasya@users.noreply.github.com>
2020-03-18 20:09:58 -0400

    fix alignment of actual values (predicted still needs fixing due to changes in the way windows are created)

--- rt_prediction_visualization_buggy.py
+++ rt_prediction_visualization_buggy.py
@@ -7,8 +7,11 @@ Created on Sun Mar  8 16:49:41 2020
 
 """
 ****QUICK USAGE NOTE***
+- This produces several graphs of the same style but at different points
 - You can play with everything within the "CHANGE HERE" box
 - Ask me if you need any explanations!
+
+- note currently the prediction windowing is off - will fix
 """
 
 
@@ -23,21 +26,18 @@ Created on Sun Mar  8 16:49:41 2020
 
 # TO DO:
 # Match the colours of finger label to channel
-# Change filtering to be window-wise to be consistent with the real time model
 # When everything works, make the code beautiful
 """
 
 from real_time_class import RealTimeML
 # import from siggy directory
-from siggy.match_labels import append_labels, filter_dataframe, label_window
+from siggy.match_labels import load_data, filter_dataframe, create_windows
 import numpy as np
 import matplotlib.pyplot as plt
 
 channels = [1,2,3,4,5,6,7,8,13]
 
-##############################################
 ###### YOU CAN CHANGE FROM HERE ##############
-
 length = 250
 shift = 0.5 * length
 
@@ -48,7 +48,6 @@ channel_names = ['channel {}'.format(i) for i in channels[:-1]]
 # Keeping it simple just so we can fix plots
 feature_names = ['wl']
 
-
 # When I try early data I get a list index out of range error in append_labels
 # but these ones seem to work
 # also note that you need to use self directed or guided trials in order to see
@@ -58,13 +57,13 @@ label_file = 'data/2020-03-08/012_trial2_self-directed_2020-03-08-19-06-09-042.t
 
 # TO HERE ####################################
 #%%
-res = append_labels(data_file, label_file, channels)
+res = load_data(data_file, label_file, channels)
 data = filter_dataframe(res)        # LATER MAKE THIS USING FILTER BY WINDOW
 
 # take everything means don't cut off the empty stuff at the start
 # shift means have a window start every 0.5 seconds
 # offset is like if we want to trim margins from start and end of the entire trace
-windows = label_window(data, shift=shift, offset=0, take_everything=True)
+windows = create_windows(data, shift=shift, offset=0, take_everything=True)
 
 # for now we make windows into a numpy array cause it's not liking the df
 # We also only take the first 10000 to speed it up
@@ -92,24 +91,30 @@ labels['timestamp(ms)'] = labels['timestamp(ms)'] - data['timestamp(ms)'].iloc[0
 
 # do several plots of the same style but at different time intervals 
 # Here we go for traces of length 20s
-for start_sec in [i*20 for i in range(8, 12)]:
+for start_sec in [i*10 for i in range(20, 26)]:
     start = length * start_sec
-    end = start + length*20
+    end = start + (length * 10)
     #%%
     # now we have the predictions, need to plot it against all the channels
     fig,(ax1,ax2,ax3,ax4,ax5) = plt.subplots(5,1,figsize=(20,15),sharex=True)
 
     emg = data.to_numpy()
     
+    
+    # try against explicit index?
+    x = np.linspace(start,end,num=(end-start))
+    
+    
+    
     # for each hand plot spahetti line plot
     ax1 = plt.subplot(5,1,1)
     for ch in range(0,4):
-        ax1.plot(emg[start:end,ch])
+        ax1.plot(x,emg[start:end,ch])
     ax1.set_title('hand one')
         
     ax2 = plt.subplot(5,1,2, sharex=ax1, sharey=ax1)
     for ch in range(4,8):
-        ax2.plot(emg[start:end,ch])
+        ax2.plot(x,emg[start:end,ch])
     ax2.set_title('hand two')
     
     #%%
@@ -133,7 +138,7 @@ for start_sec in [i*20 for i in range(8, 12)]:
     
     # fit pred_vals to window that we're displaying
     fitted_pred_vals = pred_vals[int(start/shift):int(end/shift)];
-    fitted_pred_vals[fitted_pred_vals != 0] -= start;
+    #fitted_pred_vals[fitted_pred_vals != 0] -= start;
     
     # Now make eventplot for predicted values
     # RIGHT FLUSH MEANS THAT THE PREDICTION IS PLACED AT THE END OF THE WINDOW
@@ -165,70 +170,36 @@ for start_sec in [i*20 for i in range(8, 12)]:
             ax4.text(int(m),1,str(clas))
     #%%
     # Now add actual labels over the predicted
-            
-            
-    # thinking
-    """
-    So in data aka emg, each 250 index is 1 second
-    250 i = 1 s
-    in labels, the timestamps need to be divided by 1000 to be in seconds
-    let j be in the value in the timestamp(ms)
-    1 j = 1000 ms => 1/1000 * j = 1 ms
-    1000 ms = 1 s 
-    But then the times need to
-    1000 ms = 250 i
-    1000/250  * ms = i
-    (1/1000) * (1000/250) j = i => 1/250
-    So really we just need to divide it by 250
-    
-    """
-            
-    ## Make numpy for graphing like we did for emg
-    # we do start/length*1000 to get the time in milliseconds
-    # which SHOULD correpond to timestamp but doesn't cause life sux
-    fitted_labels = labels
-    
-    #fitted_labels['timestamp'] = fitted_labels['timestamp(ms)'] / 1000
-    #fitted_labels.drop(columns='timestamp(ms)')
-    
+        
     print('start in ms is', start / length * 1000)
-    print('end in ms is', (end / length * 1000))
+    print('end in ms is', end / length * 1000)
     
+    ## Make numpy for graphing like we did for emg
+    fitted_labels = labels
+    # we do start/length*1000 to get the time in milliseconds
     fitted_labels = fitted_labels[fitted_labels['timestamp(ms)'] >= (start / length * 1000)]
     fitted_labels = fitted_labels[fitted_labels['timestamp(ms)'] <= (end / length * 1000)]
     
     # make the index line up
-    
-    # see this is where everything goes wrong sadness
-    fitted_labels['timestamp(ms)'] -= (start / length * 1000) 
-    fitted_labels['timestamp(ms)'] *= 1000
-    fitted_labels['timestamp(ms)'] /= 250
-    
-    
-
+    fitted_labels['timestamp(ms)'] /= 1000 # to seconds
+    fitted_labels['timestamp(ms)'] *= 250 # 1s = 250 points, 
     fitted_labels = fitted_labels.to_numpy()    
-    # see you just need to ask yourself why the hell you didn't make
-    # the horizontal axis the time in seconds to begin with
-    
+
     #%%
     
     # Now plot actual labels
     ax5 = plt.subplot(5,1,5,sharex=ax1)
     ax5.set_title('actual keypresses')
 
-    #ax5.eventplot(fitted_labels[:,0], colors=color[fitted_labels[:,1]-1])
-    # ignore colours for now
-    ax5.eventplot(fitted_labels[:,0])
+    ax5.eventplot(fitted_labels[:,0])# add colour codinglater
     
-    """
-                                             
     # and put markers of uniform colour above event lines
     for m, time in enumerate(fitted_labels[:,0]):
         #plot text (finger number AND character)
-        ax5.text(time,1,fitted_labels[m,1])
-        ax5.text(time,1.5,fitted_labels[m,2])
+        ax5.text(time,1,str(fitted_labels[m,1]))
+        ax5.text(time,1.5,str(fitted_labels[m,2]))
         
-    """
+    
     plt.show()
     #%%
             
