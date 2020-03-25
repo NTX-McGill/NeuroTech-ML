d7a8f79fb9f3b30dc833d3bb460db73043793d7a
Anna Brandenberger <anna.brandenberger@mail.mcgill.ca>
2020-03-25 11:02:12 -0400

    update finger mapping, generate realistic siggy output in test_generation.py

--- siggy/finger_letter_mapping.py
+++ siggy/finger_letter_mapping.py
@@ -5,51 +5,53 @@
 
 
 # author @miasya
-# last modified feb 9, 2020
+# last modified mar 22, 2020
 
 # DESC: This file defines 2 dicts with letter to finger AND finger to letter index mapping
 
 # Our data will produce a vector of length 10, where index
-# 0 - left - pinky
-# 1 - left - ring
-# 2 - left - middle
-# 3 - left - index
-# 4 - SPACE
-# 5 - right - pinky
-# 6 - right - ring
-# 7 - right - middle
-# 8 - right - index
+# 0 - no signal
+# 1 - right - thumb (SPACE)
+# 2 - right - index 
+# 3 - right - middle 
+# 4 - right - ring 
+# 5 - right - pinky 
+# 6 - left - thumb
+# 7 - left - index 
+# 8 - left - middle 
+# 9 - left - ring 
+# 10 - left - pinky
 
-letter_finger = {'q' : 0, 'a' : 0, 'z' : 0,
-                'w' : 1, 's' : 1, 'x' : 1,
-                'e' : 2, 'd' : 2, 'c' : 2,
-                'r' : 3, 'f' : 3, 'v' : 3, 't' : 3, 'g' : 3, 'b' : 3,
+letter_finger = {'q' : 10, 'a' : 10, 'z' : 10,
+                'w' : 9, 's' : 9, 'x' : 9,
+                'e' : 8, 'd' : 8, 'c' : 8,
+                'r' : 7, 'f' : 7, 'v' : 7, 't' : 7, 'g' : 7, 'b' : 7,
                 'p' : 5,
-                'o' : 6, 'l' : 6,
-                'i' : 7, 'k' : 7,
-                'u' : 8, 'j' : 8, 'm' : 8, 'y' : 8, 'h' : 8, 'n' : 8, 
-                ' ' : 4}
+                'o' : 4, 'l' : 4,
+                'i' : 3, 'k' : 3,
+                'u' : 2, 'j' : 2, 'm' : 2, 'y' : 2, 'h' : 2, 'n' : 2, 
+                ' ' : 1}
 
-finger_letter = {0 : ('q', 'a', 'z'),
-                1 : ('w', 's', 'x'),
-                2 : ('e', 'd', 'c'),
-                3 : ('r', 'f', 'v', 't', 'g', 'b'),
-                4 : ' ',
+finger_letter = {10 : ('q', 'a', 'z'),
+                9 : ('w', 's', 'x'),
+                8 : ('e', 'd', 'c'),
+                7 : ('r', 'f', 'v', 't', 'g', 'b'),
+                1 : ' ',
                 5 : ('p'),
-                6 : ('o', 'l'),
-                7 : ('i', 'k'),
-                8 : ('u', 'j', 'm', 'y', 'h', 'n')}
+                4 : ('o', 'l'),
+                3 : ('i', 'k'),
+                2 : ('u', 'j', 'm', 'y', 'h', 'n')}
 
 
 """
 # Sample code
-my_sentence = "do you like pancakes"
-
-for c in my_sentence:
-    if c in letter_finger.keys(): 
-        print(letter_finger[c])
 """
+if __name__ == "__main__": 
+    my_sentence = "do you like pancakes"
 
+    for c in my_sentence:
+        if c in letter_finger.keys(): 
+            print(letter_finger[c])
 
 # In[ ]:
 
