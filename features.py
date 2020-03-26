4bef1ee4abb11716b2d25bc77d06050bbd1d37e7
dcxSt <stephen.fay@mail.mcgill.ca>
2020-03-25 21:37:44 -0400

    updates features to make them relevant to the new filter

--- features.py
+++ features.py
@@ -101,7 +101,8 @@ def get_psd(signal):
 
 #Returns list of bands that you can use
 def get_bands(psd):
-    return [psd[5:20] , psd[20:40] , psd[40:60] , psd[80:100] , psd[100:120]]
+    # return [psd[5:20] , psd[20:40] , psd[40:80] , psd[80:100], psd[100:120]]
+    return [psd[5:11] , psd[11,15] , psd[21:29] , psd[29:36] , psd[36:43] , psd[43:50]]# , psd[80:100] , psd[100:120]]
 
 ###
 
